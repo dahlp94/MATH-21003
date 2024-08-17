c95cb47f615b4188600125870bb05ea700eaee40
Pratik Dahal <mapratikdahal@gmail.com>
2024-08-17 12:36:41 -0500

    simulating some coin flips

---
+++ frequentist_probability_simulation.R
@@ -0,0 +1,5 @@
+# We will simulate flipping a fair coin 10000 times and calculate 
+# the probability of getting heads.
+# H: Head  T: Tail
+# We will perform sample with replacement
+sample(c("H", "T"), size = 10000, replace = TRUE)
