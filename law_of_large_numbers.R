b487b6d2e5a524d554d32864576f7f8866d22295
Pratik Dahal <mapratikdahal@gmail.com>
2024-08-22 01:55:58 -0500

    added simulation for different sample sizes

--- law_of_large_numbers.R
+++ law_of_large_numbers.R
@@ -6,3 +6,15 @@ true_mean <- 10
 true_sd <- 2
 
 # Simulation
+simulate_mean <- function(n) {
+  samples <- rnorm(n, mean = true_mean, sd = true_sd)
+  return(mean(samples))
+}
+
+# Simulating for different sample sizes
+sample_sizes <- c(10, 100, 1000, 10000, 100000)
+results <- sapply(sample_sizes, simulate_mean)
+
+data.frame(Sample_Sizes = sample_sizes, Sample_Mean = results)
+
+
